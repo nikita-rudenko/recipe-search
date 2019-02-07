c10ef7e1e97f8ab00a09aa9be5c159e6fed1395a
Nikita R <rdnkta@gmail.com>
2019-02-07 20:18:42 +0200

    RecipeList component setup

--- src/App.js
+++ src/App.js
@@ -1,32 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
-import recipes from './data/tempList';
+import { recipes } from './data/tempList';
 import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.min.css';
 
 class App extends Component {
 	state = {
-		recipes: [],
-		url: process.env.REACT_APP_FOOD2FORK_API
+		recipes: recipes
+		// url: process.env.REACT_APP_FOOD2FORK_API
 	};
 
-	async getRecipes() {
-		try {
-			const data = await fetch(this.state.url);
-			const jsonData = await data.json();
+	// async getRecipes() {
+	// 	try {
+	// 		const data = await fetch(this.state.url);
+	// 		const jsonData = await data.json();
 
-			this.setState({
-				recipes: jsonData.recipes
-			});
-		} catch (error) {
-			console.log(error);
-		}
-	}
+	// 		this.setState({
+	// 			recipes: jsonData.recipes
+	// 		});
+	// 	} catch (error) {
+	// 		console.log(error);
+	// 	}
+	// }
+
+	// componentDidMount() {
+	// 	this.getRecipes();
+	// }
 
 	render() {
+		// console.log(this.state.recipes);
 		return (
 			<>
-				<RecipeList />
+				<RecipeList recipes={this.state.recipes} />
 				<RecipeDetails />
 			</>
 		);
--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -4,12 +4,23 @@ import RecipeSearch from './RecipeSearch';
 
 export default class RecipeList extends Component {
 	render() {
+		const { recipes } = this.props;
 		return (
-			<div>
-				<h1>Hello List</h1>
+			<>
 				<RecipeSearch />
-				<Recipe />
-			</div>
+				<div className="container my-5">
+					<div className="row">
+						<div className="col-10 mx-auto col-md-6 text-center mb-3">
+							<h1>Recipe List</h1>
+						</div>
+					</div>
+					<div className="row">
+						{recipes.map(recipe => {
+							return <Recipe key={recipe.recipe_id} recipe={recipe} />;
+						})}
+					</div>
+				</div>
+			</>
 		);
 	}
 }
