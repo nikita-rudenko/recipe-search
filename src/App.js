0d9638280f54f12a23501d4cc420ca854ade562e
Nikita R <rdnkta@gmail.com>
2019-02-07 18:45:30 +0200

    set api call

--- src/App.js
+++ src/App.js
@@ -5,6 +5,24 @@ import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
 
 class App extends Component {
+	state = {
+		recipes: [],
+		url: process.env.REACT_APP_FOOD2FORK_API
+	};
+
+	async getRecipes() {
+		try {
+			const data = await fetch(this.state.url);
+			const jsonData = await data.json();
+
+			this.setState({
+				recipes: jsonData.recipes
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
 	render() {
 		return (
 			<>
